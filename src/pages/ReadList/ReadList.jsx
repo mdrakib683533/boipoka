ab4288426a168b4365339ef625c9767626cd1303
rakibhossen123 <osmangoni87367@gmail.com>
2025-05-03 02:46:56 +0600

    added the fav icon

--- src/pages/ReadList/ReadList.jsx
+++ src/pages/ReadList/ReadList.jsx
@@ -4,6 +4,7 @@ import { Tab, Tabs, TabList, TabPanel } from "react-tabs";
 import "react-tabs/style/react-tabs.css";
 import { getStoredBook } from "../../utility/addToDb";
 import Book from "../Book/Book";
+import { Helmet } from "react-helmet-async";
 
 const ReadList = () => {
   const [readList, setReadList] = useState([]);
@@ -21,41 +22,46 @@ const ReadList = () => {
     setReadList(myReadList);
   }, []);
 
-  const handleSort = (type)=>{
+  const handleSort = (type) => {
     setSort(type);
-  }
+  };
 
   return (
-    <div className="mt-20">
-      <details className="dropdown">
-        <summary className="btn m-1">sort by: {sort? sort: ''}</summary>
-        <ul className="menu dropdown-content bg-base-100 rounded-box z-1 w-52 p-2 shadow-sm">
-          <li>
-            <a onClick={()=> handleSort('pages')}>pages</a>
-          </li>
-          <li>
-            <a onClick={()=> handleSort('ratings')}>ratings</a>
-          </li>
-        </ul>
-      </details>
-      <Tabs>
-        <TabList>
-          <Tab>Read Book List</Tab>
-          <Tab>My WishList</Tab>
-        </TabList>
+    <div>
+      <Helmet>
+        <title>listed Books</title>
+      </Helmet>
+      <div className="mt-20">
+        <details className="dropdown">
+          <summary className="btn m-1">sort by: {sort ? sort : ""}</summary>
+          <ul className="menu dropdown-content bg-base-100 rounded-box z-1 w-52 p-2 shadow-sm">
+            <li>
+              <a onClick={() => handleSort("pages")}>pages</a>
+            </li>
+            <li>
+              <a onClick={() => handleSort("ratings")}>ratings</a>
+            </li>
+          </ul>
+        </details>
+        <Tabs>
+          <TabList>
+            <Tab>Read Book List</Tab>
+            <Tab>My WishList</Tab>
+          </TabList>
 
-        <TabPanel>
-          <h2>Book i read {readList.length}</h2>
-          <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-6 mb-10 w-6xl mx-auto">
-            {readList.map((b) => (
-              <Book key={b.bookId} singleBook={b}></Book>
-            ))}
-          </div>
-        </TabPanel>
-        <TabPanel>
-          <h2>My WishList</h2>
-        </TabPanel>
-      </Tabs>
+          <TabPanel>
+            <h2>Book i read {readList.length}</h2>
+            <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-6 mb-10 w-6xl mx-auto">
+              {readList.map((b) => (
+                <Book key={b.bookId} singleBook={b}></Book>
+              ))}
+            </div>
+          </TabPanel>
+          <TabPanel>
+            <h2>My WishList</h2>
+          </TabPanel>
+        </Tabs>
+      </div>
     </div>
   );
 };
